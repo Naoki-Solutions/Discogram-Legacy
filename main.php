5a491ded1e799967f3e1bcb1344474316f9db4dd
KeyderDev <ajomar321@gmail.com>
2023-02-13 18:36:50 -0400

    Changed UI (main.php)

--- main.php
+++ main.php
@@ -28,7 +28,7 @@
     <a href="./index.php"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-solid fa-house"></i></h1></a>
     <a href="https://discord.gg/YvFGRAw2wx"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-brands fa-discord"></i></h1></a>
     <a href="culto_del_pan.php"><img style="width:40px;background-color:#21252b" src="bread.png"></a>
-    <a href="./settings"><h1 style="margin:0;font-size:20px;padding:28rem;padding-left:14px;"><i style="margin:0;color:#85898f;" class="fa-solid fa-gear"></i></h1></a>
+    <a href="./settings.php"><h1 style="margin:0;font-size:20px;padding:28rem;padding-left:14px;"><i style="margin:0;color:#85898f;" class="fa-solid fa-gear"></i></h1></a>
 </div>
 
 <div class="test2">
