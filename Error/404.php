bc3c245006683020c2b71ece2171c23da8b9a869
KeyderDev <ajomar321@gmail.com>
2023-02-13 21:02:05 -0400

    Some Changes

---
+++ Error/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title>404</title>
+</head>
+<body>
+<div class="ruido paa z-2"> </div>
+<div class="paa z-1" style="background: radial-gradient(rgb(0 0 0 / 7%),rgb(0 0 0 / 30%));" id="oclucion_fondo"> </div>
+<div style="margin: 4em 6em; max-width: 50em;">
+<div class="">
+<h1 style="font-size: 4.4rem;margin-bottom: 0px;">ERROR 404</h1>
+<h2 style="margin-top: 0.1em; margin-bottom: 3em;">NOT FOUND</h2>
+<h2 style="font-size: 1.3em; line-height: 27px; font-weight: normal;">It seems that whatever you are looking for is not found, if you think something should be here, you can contact support by sending an email to <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="c0b3b5b0b0afb2b480b8a5b6aea5b4eea3afadee">[email&#160;protected]</a></h2>
+<h2 style="font-size: 1.3em; line-height: 27px; font-weight: normal;"><u><a href="#1">Go back</a></u> or <a href="#1">return to the homepage</a>.</h2>
+<h2 style="font-size: 1.3em; line-height: 27px; font-weight: normal;">Good luck.</h2>
+</div>
+</div>
+<style media="screen">
+@font-face { font-family: 'Inconsolata'; src: url(/assets/font/inconsolata.ttf); }
+
+body{font-family: 'Inconsolata', monospace; background-color: #21252b;color:#8f8f8f}
+a{color:#8f8f8f;}
+.a01{}
+.m0{margin: 0;} .dfac{display: flex;justify-content: center; align-items: center;} .z-2{z-index: -2;} .z-1{z-index: -1;} .z0{z-index: 0;} .z1{z-index: 1;} .z2{z-index: 2;} .z3{z-index: 3;} .paa, .paf{position: absolute; left: 0px; right: 0px; bottom: 0px; top: 0px;}
+
+</style>
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script></body>
+</html>
