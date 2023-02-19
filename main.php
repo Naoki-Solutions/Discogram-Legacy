78647785d22127f6544c7bf3e2e1d450a4df0dff
KeyderDev <ajomar321@gmail.com>
2023-02-18 21:35:06 -0400

    Some Changes

--- main.php
+++ main.php
@@ -51,7 +51,7 @@
 <div class="test2">
 <h4 style="padding-left:20px;"><i class="fa-solid fa-comments"></i> Chat Global</h4>
 <a href="./informacion" style="color:#85898f;"><h4 style="padding-left:20px;"><i class="fa-solid fa-circle-info"></i> Informacion</h4></a>
-<h4 style="padding-left:20px;"><i class="fa-solid fa-map"></i> Explorar</h4>
+<a href="./explorar" style="color:#85898f;"><h4 style="padding-left:20px;"><i class="fa-solid fa-map"></i> Explorar</h4></a>
 <h4 style="padding-left:20px;"><i class="fa-solid fa-shop"></i> Tienda</h4>
 <h4 style="padding-left:20px;"><i class="fa-solid fa-lock"></i> Admin Room</h4>
 <hr>
