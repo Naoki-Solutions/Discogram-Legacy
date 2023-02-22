6fb647ce177022ccb04049973a0f3cce9749f7a6
KeyderDev <ajomar321@gmail.com>
2023-02-22 19:22:43 -0400

    Changes

--- radio.php
+++ radio.php
@@ -11,37 +11,22 @@
 </head>
 <body>
 <div class="bg_1 h_aba df fs0"><div class="br3 df mw_abt aic pl10 pt2 fg1 pto03 ns"><span class="f13 ml7">Naoki OnNet</span></div><div class="df fs0 aic"><div class="dfac p0 bg_2 br mt5 mb5 ml1 mr1 nb_if_nh cp ns"><div class="w11 h11 br2 b2s co_4 df pr m3"><div class="df pa t-1 l-1 r-1 b-1"><div class="fg1 bgcc co_4 br2s"></div><div class="fg1 pl4"></div></div></div></div><div class="dfac p0 bg_2 br mt5 mb5 ml1 mr1 nb_if_nh cp ns"><div class="w11 h11 br2 b2s co_4 df pr m3"><div class="df pa t-1 l-1 r-1 b-1"><div class="fg1 pr4"></div><div class="fg1 bgcc co_4 bl2s"></div></div></div></div><div class="h16 bgcc co_4 w2 br2 ml5 mr5"></div><div class="dfac p0 bg_2 br mt5 mb5 ml1 mr1 nb_if_nh cp ns"><div class="w15 h15 br2 df pr gg2 m3"><div class="fg1 dfc"><div class="fg1 b2s co_4 br2"></div></div><div class="fg1 dfc gg2"><div class="fg1 b2s co_4 br2"></div><div class="fg1 b2s co_4 br2"></div></div></div></div></div><div class="hba01 df fs0 ml20"><div class="dfac fg1 pto02" id="botonMinimizarVentana"><svg width="29" height="30" version="1.1" viewBox="0 0 7.9375 7.9375"><rect x="2.3812" y="3.8365" width="3.075" height=".26458" fill="currentColor" stroke-width=".2581"></rect></svg></div><div class="dfac fg1 pto02" id="botonMaximizarVentana"><svg width="28" height="28" version="1.1" viewBox="0 0 7.9375 7.9375"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width=".22388px"><path d="m2.6255 5.312h2.2388v-2.2388h-2.2388z"></path><path d="m5.312 4.7523v-2.1268h-2.1268"></path></g></svg></div><div class="dfac fg1 pto01" id="botonCerrarVentana"><svg width="23" height="23" version="1.1" viewBox="0 0 7.9375 7.9375"><path d="m2.3812 5.5562 3.175-3.175m-3.175 0 3.175 3.175" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width=".42917"></path></svg></div></div></div>
+
 <div class="maincontainer">
     <h1 style="padding-left:6rem;font-family: 'Inconsolata', monospace;">Naoki OnNet</h1>
-    <h2 style="padding-left:6rem;">Landing</h2>
+    <h2 style="padding-left:6rem;">Radio</h2>
     <p style="padding-left:6rem;margin:0;">Aqui podras encontra una lista de servidores donde podras chatear.</p>
     <p style="padding-left:6rem;margin:0;">Si encuentras algun bug, no olvides reportarlo en nuestro servidor de Discord.</p>
-    <h2 style="padding-left:6rem;">Servidores Recomendados</h2>
     <hr>
-    <h3 style="padding-left:6rem;">El Culto del Pan</h3>
-    <p style="padding-left:6rem;margin:0;">Este es un grupo donde charlarmos sobre el pan, y lo alabamos.</p>
-    <br>
-    <p style="padding-left:6rem;margin:0;">Creado: 14/2/2023 (14 feb 2023)</p>
-    <p style="padding-left:6rem;margin:0;">Estado: <span style="color:green">Abierto</span></p>
-    <p style="padding-left:6rem;margin:0;">Actividad: 0%</p>
-    <br>
-    <a href="./culto_del_pan"><span style="padding-left:6rem;margin:0;color:green;font-family: 'Inconsolata', monospace;">Click aqui para acceder</span></a>
-    <hr>
-    <h3 style="padding-left:6rem;">Main Developers</h3>
-    <p style="padding-left:6rem;margin:0;">En este gremio compartimos cosas sobre la programacion.</p>
-    <br>
-    <p style="padding-left:6rem;margin:0;">Creado: 14/2/2023 (14 feb 2023)</p>
-    <p style="padding-left:6rem;margin:0;">Estado: <span style="color:red">Cerrado</span></p>
-    <p style="padding-left:6rem;margin:0;">Actividad: 0%</p>
-    <br>
-    <a href="./main_developers"><span style="padding-left:6rem;margin:0;color:green;font-family: 'Inconsolata', monospace;">Click aqui para acceder</span></a>
-    <br>
-    <hr>
-</div>
+    <h3 style="padding-left:6rem;">Naoki Radio - 100.7 FM</h3>
+    <p style="padding-left:6rem;">Status: <span style="color:green;">On Air</span></p>
+    <audio controls style="width:20rem;padding-left:5rem;">
+    <source src="http://192.168.0.18:8000/mystream.ogg" type="audio/ogg">
+</audio>
 </div>
 
 <div class="test">
-    <a href="./index"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-solid fa-house"></i></h1></a>
+    <a href="./main"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-solid fa-house"></i></h1></a>
     <a href="./explorar"><h1 style="font-size:20px;padding-left:12px"><i style="color:#85898f;" class="fa-solid fa-compass"></i></h1></a>
     <a href="./herramientas"><h1 style="font-size:20px;padding-left:12px"><i style="color:#85898f;" class="fa-solid fa-wrench"></i></h1></a>
     <a href="./radio"><h1 style="font-size:20px;padding-left:13px"><i style="color:#85898f;" class="fa-solid fa-radio"></i></h1></a>
@@ -82,13 +67,6 @@
     <h4 style="padding-left:10px;margin:8px;">Websocket (Bus)</h4>
     <p style="margin:0;padding-left:15px;font-size:13px;">Conectado: <span style="color:red">false</span></p>
     <p style="margin:0;padding-left:15px;font-size:13px;">Puerto: <span style="color:red">none</span></p>
-    <p style="margin:0;padding-left:15px;font-size:13px;">Bus URL: <span style="color:red">undefined</span></p>
-</div>
-
-<div class="debug3">
-<h4 style="padding-left:10px;margin:8px;"><i class="fa-solid fa-server"></i> Advertencia</h4>
-<p style="padding-left:10px;margin:8px;font-size:12x;font-family:sans-serif;">Es posible que el servicio de <b style="font-size:14px;">WebSockets</b> no se encuentre disponible.</p>
-<div class="line"></div>
 </div>
 
 </div>
--- users.php
+++ users.php
@@ -11,41 +11,25 @@
 </head>
 <body>
 <div class="bg_1 h_aba df fs0"><div class="br3 df mw_abt aic pl10 pt2 fg1 pto03 ns"><span class="f13 ml7">Naoki OnNet</span></div><div class="df fs0 aic"><div class="dfac p0 bg_2 br mt5 mb5 ml1 mr1 nb_if_nh cp ns"><div class="w11 h11 br2 b2s co_4 df pr m3"><div class="df pa t-1 l-1 r-1 b-1"><div class="fg1 bgcc co_4 br2s"></div><div class="fg1 pl4"></div></div></div></div><div class="dfac p0 bg_2 br mt5 mb5 ml1 mr1 nb_if_nh cp ns"><div class="w11 h11 br2 b2s co_4 df pr m3"><div class="df pa t-1 l-1 r-1 b-1"><div class="fg1 pr4"></div><div class="fg1 bgcc co_4 bl2s"></div></div></div></div><div class="h16 bgcc co_4 w2 br2 ml5 mr5"></div><div class="dfac p0 bg_2 br mt5 mb5 ml1 mr1 nb_if_nh cp ns"><div class="w15 h15 br2 df pr gg2 m3"><div class="fg1 dfc"><div class="fg1 b2s co_4 br2"></div></div><div class="fg1 dfc gg2"><div class="fg1 b2s co_4 br2"></div><div class="fg1 b2s co_4 br2"></div></div></div></div></div><div class="hba01 df fs0 ml20"><div class="dfac fg1 pto02" id="botonMinimizarVentana"><svg width="29" height="30" version="1.1" viewBox="0 0 7.9375 7.9375"><rect x="2.3812" y="3.8365" width="3.075" height=".26458" fill="currentColor" stroke-width=".2581"></rect></svg></div><div class="dfac fg1 pto02" id="botonMaximizarVentana"><svg width="28" height="28" version="1.1" viewBox="0 0 7.9375 7.9375"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width=".22388px"><path d="m2.6255 5.312h2.2388v-2.2388h-2.2388z"></path><path d="m5.312 4.7523v-2.1268h-2.1268"></path></g></svg></div><div class="dfac fg1 pto01" id="botonCerrarVentana"><svg width="23" height="23" version="1.1" viewBox="0 0 7.9375 7.9375"><path d="m2.3812 5.5562 3.175-3.175m-3.175 0 3.175 3.175" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width=".42917"></path></svg></div></div></div>
+
 <div class="maincontainer">
     <h1 style="padding-left:6rem;font-family: 'Inconsolata', monospace;">Naoki OnNet</h1>
-    <h2 style="padding-left:6rem;">Landing</h2>
-    <p style="padding-left:6rem;margin:0;">Aqui podras encontra una lista de servidores donde podras chatear.</p>
-    <p style="padding-left:6rem;margin:0;">Si encuentras algun bug, no olvides reportarlo en nuestro servidor de Discord.</p>
-    <h2 style="padding-left:6rem;">Servidores Recomendados</h2>
+    <h2 style="padding-left:6rem;">Usuarios de Naoki OnNet</h2>
+    <p style="padding-left:6rem;margin:0;">Aqui podras encontrar los usuarios que participaron de alguna forma</p>
+    <p style="padding-left:6rem;margin:0;">en el desarrollo de Naoki OnNet.</p>
     <hr>
-    <h3 style="padding-left:6rem;">El Culto del Pan</h3>
-    <p style="padding-left:6rem;margin:0;">Este es un grupo donde charlarmos sobre el pan, y lo alabamos.</p>
-    <br>
-    <p style="padding-left:6rem;margin:0;">Creado: 14/2/2023 (14 feb 2023)</p>
-    <p style="padding-left:6rem;margin:0;">Estado: <span style="color:green">Abierto</span></p>
-    <p style="padding-left:6rem;margin:0;">Actividad: 0%</p>
-    <br>
-    <a href="./culto_del_pan"><span style="padding-left:6rem;margin:0;color:green;font-family: 'Inconsolata', monospace;">Click aqui para acceder</span></a>
-    <hr>
-    <h3 style="padding-left:6rem;">Main Developers</h3>
-    <p style="padding-left:6rem;margin:0;">En este gremio compartimos cosas sobre la programacion.</p>
-    <br>
-    <p style="padding-left:6rem;margin:0;">Creado: 14/2/2023 (14 feb 2023)</p>
-    <p style="padding-left:6rem;margin:0;">Estado: <span style="color:red">Cerrado</span></p>
-    <p style="padding-left:6rem;margin:0;">Actividad: 0%</p>
-    <br>
-    <a href="./main_developers"><span style="padding-left:6rem;margin:0;color:green;font-family: 'Inconsolata', monospace;">Click aqui para acceder</span></a>
-    <br>
-    <hr>
+    <div class="user1">
+    <span style="padding:10px;margin:0;top:100px">xKeyder_#0425</span>
 </div>
+
+
+
 </div>
 
 <div class="test">
-    <a href="./index"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-solid fa-house"></i></h1></a>
+    <a href="./main"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-solid fa-house"></i></h1></a>
     <a href="./explorar"><h1 style="font-size:20px;padding-left:12px"><i style="color:#85898f;" class="fa-solid fa-compass"></i></h1></a>
     <a href="./herramientas"><h1 style="font-size:20px;padding-left:12px"><i style="color:#85898f;" class="fa-solid fa-wrench"></i></h1></a>
-    <a href="./radio"><h1 style="font-size:20px;padding-left:13px"><i style="color:#85898f;" class="fa-solid fa-radio"></i></h1></a>
-    <a href="./users"><h1 style="font-size:20px;padding-left:13px"><i style="color:#85898f;" class="fa-solid fa-user"></i></h1></a>
     <a href="https://discord.gg/YvFGRAw2wx"><h1 style="font-size:20px;padding-left:10px"><i style="color:#85898f;" class="fa-brands fa-discord"></i></h1></a>
     <a href="main_developers"><img style="width:40px;background-color:#21252b" src="./assets/images/gdevtrans.png"></a>
     <a href="./culto_del_pan"><img style="width:40px;background-color:#21252b" src="./assets/images/bread.png"></a>
@@ -82,13 +66,6 @@
     <h4 style="padding-left:10px;margin:8px;">Websocket (Bus)</h4>
     <p style="margin:0;padding-left:15px;font-size:13px;">Conectado: <span style="color:red">false</span></p>
     <p style="margin:0;padding-left:15px;font-size:13px;">Puerto: <span style="color:red">none</span></p>
-    <p style="margin:0;padding-left:15px;font-size:13px;">Bus URL: <span style="color:red">undefined</span></p>
-</div>
-
-<div class="debug3">
-<h4 style="padding-left:10px;margin:8px;"><i class="fa-solid fa-server"></i> Advertencia</h4>
-<p style="padding-left:10px;margin:8px;font-size:12x;font-family:sans-serif;">Es posible que el servicio de <b style="font-size:14px;">WebSockets</b> no se encuentre disponible.</p>
-<div class="line"></div>
 </div>
 
 </div>
