fba0893c52bbae1e39d3282138b5222a005cc97f
KeyderDev <ajomar321@gmail.com>
2023-04-05 21:33:15 -0300

    Changes

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,7 +1,7 @@
 <?php      
     $host = "127.0.0.1";  
     $user = "root";  
-    $password = '';  
+    $password = 'ab3135c2@';  
     $db_name = "login";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
